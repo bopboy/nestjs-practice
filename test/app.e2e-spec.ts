e3ed533f7d8296cd2e23bc2cc7662c6aa0e2c89b
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-24 18:56:55 +0900

    22 post, patch, delete 테스트

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -27,27 +27,24 @@ describe('AppController (e2e)', () => {
       .expect('Welcome to My Movie API');
   });
 
-  it('/movies (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/movies').expect(200).expect([])
-  })
-
   describe('/movies', () => {
     it('GET', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([])
     })
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer()).post('/movies').send({ title: 'test', year: 2000, genre: ['test'] }).expect(201)
     })
+    it('POST 400', () => {
+      return request(app.getHttpServer()).post('/movies').send({ title: 'test', year: 2000, genre: ['test'], other: 's' }).expect(400)
+    })
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/moives').expect(404)
+    })
   })
-  it('/movies (DELETE)', () => {
-    return request(app.getHttpServer()).delete('/moives').expect(404)
-  })
-
   describe('/movies/:id', () => {
     it('GET 200', () => { return request(app.getHttpServer()).get('/movies/1').expect(200) })
     it('GET 404', () => { return request(app.getHttpServer()).get('/movies/999').expect(404) })
-    it.todo('DELETE')
-    it.todo('PATCH')
+    it('PATCH 200', () => { return request(app.getHttpServer()).patch('/movies/1').send({ title: 'Updated Test' }).expect(200) })
+    it('DELETE 200', () => { return request(app.getHttpServer()).delete('/movies/1').expect(200) })
   })
 });
