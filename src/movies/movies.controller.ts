98598bcb6a9794b34736a912b50246f5d02a0a94
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-24 15:55:49 +0900

    09 서비스 작성 (CRUD 중 R)

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,24 +1,26 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+    constructor(private readonly moviesService: MoviesService) { }
+
     @Get()
-    getAll() {
-        return 'This will return all movies'
+    getAll(): Movie[] {
+        return this.moviesService.getAll()
     }
     @Get('search')
     search(@Query('year') searchingYear: string) {
         return `We are searching for a movie with a title: ${searchingYear}`
     }
     @Get(':id')
-    getOne(@Param('id') movieId: string) {
-        return `This wil return one movie: ${movieId}`
+    getOne(@Param('id') movieId: string): Movie {
+        return this.moviesService.getOne(movieId)
     }
     @Post()
     create(@Body() movieData) {
-        // console.log(movieData)
-        // return 'This will create a movie'
-        return movieData
+        return this.moviesService.create(movieData)
     }
     @Delete(':id')
     remove(@Param('id') movieId: string) {
