5245df994301387d1ec95301b0c08e1003bc98b2
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-24 18:38:48 +0900

    20 e2e 테스트

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,23 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to My Movie API');
   });
+
+  it('/movies (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/movies').expect(200).expect([])
+  })
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([])
+    })
+    it('POST', () => {
+      return request(app.getHttpServer()).post('/movies').send({ title: 'test', year: 2000, genre: ['test'] }).expect(201)
+    })
+  })
+  it('/movies (DELETE)', () => {
+    return request(app.getHttpServer()).delete('/moives').expect(404)
+  })
 });
